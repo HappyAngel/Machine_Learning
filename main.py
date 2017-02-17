f0edd3eef75563c3291a7748cce31af16eedc0c1
happyangel <xiong.l@foxmail.com>
2017-02-17 16:18:28 +0800

    configs

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-class neuralNetwork:
-    
-    def __init__():
+class NeuralNetwork:
+
+    def __init__(self, inputNodes, hiddennodes, outputnodes, learningrate):
+        self.inode = inputNodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+
+        self.lr = learningrate
         pass
 
-    def train():
+    def train(self):
         pass
 
 
-    def query():
+    def query(self):
         pass
 
+if __name__ == "__main__":
+    print("hello world")
+    nn = NeuralNetwork(3,3,3,3)
